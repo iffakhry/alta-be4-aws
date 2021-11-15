3e57b06a7e4786f77c9516d990140c6cb289ff90
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2021-11-15 12:49:40 +0800

    restructuring folder

---
+++ program/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = ":80"
+	}
+	e := echo.New()
+	e.GET("/", Hello)
+	e.GET("/names/:id", HelloNames)
+	e.Start(port)
+}
+
+func Hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello world")
+}
+
+func HelloNames(c echo.Context) error {
+	name := c.Param("id")
+	return c.String(http.StatusOK, "Hello "+name)
+}
